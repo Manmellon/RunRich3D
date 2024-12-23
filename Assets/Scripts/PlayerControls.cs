1fe876da99414ebdbd90fe0a13121c60b5db6c6b
Manmellon <24492231+Manmellon@users.noreply.github.com>
2024-12-23 19:15:41 +0300

    Begin new swipe if we changed swipe direction while doing it

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -10,6 +10,8 @@ public class PlayerControls : MonoBehaviour
     public float swipeThreshold = 50f;
     //public float moveSpeed = 5f;
 
+    float swipeDirection;
+
     void Start()
     {
         player = GetComponent<Player>();
@@ -31,8 +33,18 @@ public class PlayerControls : MonoBehaviour
             case TouchPhase.Moved:
                 if (isSwiping)
                 {
+                    Vector2 swipeDelta = touch.position - startTouchPosition;
+
+                    float currentSwipeDirection = Mathf.Sign(swipeDelta.x);
+                    if (currentSwipeDirection != swipeDirection)
+                    {
+                        swipeDirection = currentSwipeDirection;
+                        startTouchPosition = currentTouchPosition;
+                    }
+
                     currentTouchPosition = touch.position;
-                    Vector2 swipeDelta = currentTouchPosition - startTouchPosition;
+                    
+                    swipeDelta = currentTouchPosition - startTouchPosition;
 
                     if (Mathf.Abs(swipeDelta.x) > swipeThreshold)
                     {
