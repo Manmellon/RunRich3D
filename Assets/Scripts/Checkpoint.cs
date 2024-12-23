2bd34ec303f6db55290977ca8e3a72781d6b94b5
Manmellon <24492231+Manmellon@users.noreply.github.com>
2024-12-23 23:21:37 +0300

    Added checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private GameObject[] flags;
+
+    void Start()
+    {
+        foreach (var flag in flags)
+            flag.transform.Rotate(transform.forward, 90);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (var flag in flags)
+            {
+                Vector3 vec = flag.transform.localEulerAngles;
+                vec.z = 0;
+                flag.transform.DOLocalRotate(vec, 0.5f);
+            }
+                
+        }
+    }
+
+}
